f4d2fdb26e3b1e250d97fa6ab4708e1cec313d1d
acoss22 <anacseq@gmail.com>
2022-07-27 23:34:23 +0100

    feat: test

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+function Category(category, items ) {
+    category, 
+    items
+}
 
 function Add(){
     var inputText = document.getElementById("category");
-    console.log(inputText.value);
+    console.log("inputText ", inputText.value);
+
+    const categ1 = new Category(inputText.value, []);
+    console.log(categ1.category);
+
+    document.getElementById("category-item").innerHTML = categ1.category;
+    
+    var categs = new Category(categ1);
+
+    for(let i = 0; i<categs.length; i++) {
+        console.log(categs[i]);
+    }
+  
 }
