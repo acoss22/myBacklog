11ef62a28a5dce3893eb009571d6137275f01863
acoss22 <anacseq@gmail.com>
2022-07-28 00:05:15 +0100

    feat: add categ

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
-function Category(category, items) {
-  category, items;
-}
-
 var myNodelist = document.getElementsByTagName("li");
 
-    // Create a new list item when clicking on the "Add" button
 function Add() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("category").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-      alert("You must write something!");
-    } else {
-      document.getElementById("myUL").appendChild(li);
+    var categoryToAdd = document.getElementById("category").value;
+    if(categoryToAdd === ""){
+        alert("You must write something!");
+        return;
     }
-    document.getElementById("myInput").value = "";
-  
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-  
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-      }
+
+    var itemsToAdd = [];
+    let categoryAdded = addCategory(categoryToAdd, itemsToAdd);
+
+    if(categoryAdded != null){
+        var li = document.createElement("li");
+        var t = document.createTextNode(categoryAdded.name);
+        li.appendChild(t);
+        
+        document.getElementById("myUL").appendChild(li);
+        document.getElementById("category").value = "";
+      
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.appendChild(txt); //span with text
+        li.appendChild(span); // add to li
+    }
+
+ 
+}
+
+let categoryList = [
+  { name: "Movies", items: [] },
+  { name: "Books", items: [] },
+  { name: "TV Shows", items: [] },
+  { name: "Games", items: [] },
+];
+
+function addCategory(category, items) {
+  for (let i = 0; i < categoryList.length; i++) {
+    if (categoryList[i].name == category) {
+      return null;
     }
   }
-
-  var inputText = document.getElementById("category");
-  console.log("inputText ", inputText.value);
-
-  const categ1 = new Category(inputText.value, []);
-
-  console.log(categ1.category);
-
-  var categs = new Category(categ1);
-
-
-
+  let newItem = { name: category, items: items };
+  categoryList.push({ name: category, items: items });
+  return newItem;
+}
